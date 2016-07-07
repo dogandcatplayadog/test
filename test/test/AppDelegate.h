db6b6a67e8602afb34a761282352647be1722abe
dogandcatplayadog <13631611448@163.com>
2016-07-07 20:43:45 +0800

    hhh

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by tang on 16/7/7.
+//  Copyright © 2016年 TangZhiHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by tang on 16/7/7.
+//  Copyright © 2016年 TangZhiHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
